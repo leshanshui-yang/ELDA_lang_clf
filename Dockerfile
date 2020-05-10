3ac4d9e2b8e452c425172ad88c9f8c240e72e0bd
Le Shan Shui Yang <leshanshui.yang@saagie.com>
2020-05-10 22:24:01 +0200

    basic docker files
    basic docker files

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+FROM python:3.7
+
+ARG PYTHON_VERSION=3.7
+
+RUN pip install numpy
+RUN pip install pandas
+
+
+RUN pip install Flask
+RUN pip install flask-restplus
+RUN pip install Flask-SSLify
+RUN pip install Flask-Admin
+RUN pip install gunicorn
+RUN pip install hdfs
+
+RUN pip install torch==1.3.0
+RUN pip install git+https://github.com/NVIDIA/apex
+RUN pip install cloudpickle==1.2.2
+
+RUN pip install transformers
+
+USER root
+
+RUN apt-get update && apt-get install -y --no-install-recommends curl
+
+COPY . /app
+WORKDIR /app
+
+EXPOSE 2333
+CMD ["gunicorn", "-b", "0.0.0.0:2333", "pred"]
---
+++ pred.py
@@ -0,0 +1,87 @@
+import os
+import pandas as pd
+import numpy as np  
+from flask import Flask, jsonify, request
+from flask import current_app, abort, Response
+import cloudpickle
+import warnings
+import logging
+import subprocess
+import transformers
+from hdfs import InsecureClient
+import importlib
+import requests
+
+
+logging.basicConfig(level=logging.INFO, format='%(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+application = Flask(__name__)
+
+
+@application.route('/config', methods=['POST'])
+def config():
+    # Read params from Json
+    if not request.json or not 'preprocess' in request.json or not 'requirements' in request.json or not 'model' in request.json:
+        abort(400)
+    preprocess_file = request.json['preprocess']
+    requirements_file = request.json['requirements']
+    model_file = request.json['model']
+    hdfs_uri = request.json['hdfs_uri']
+    logger.info('Read json configurations: OK!!')
+    
+    # Download files from HDFS
+    client_hdfs = InsecureClient('http://nn2:50070')
+    client_hdfs.download(requirements_file, "./requirements.txt", overwrite=True)
+    client_hdfs.download(model_file, "./model.pickle", overwrite=True)
+    client_hdfs.download(preprocess_file, "./preprocess.pickle", overwrite=True)
+    logger.info('Download pickles: OK!!')
+    
+    # Install library dependencies
+    subprocess.call("pip install -r ./requirements.txt", shell=True)
+    current_app.hdfs_uri = hdfs_uri
+    current_app.inMemory = False
+    current_app.configured = True
+    return jsonify({'status': "Docker configured"}), 201
+
+
+@application.route('/predict', methods=['POST'])
+def predict():
+    
+    # Read inputs/classes or Return reason of abort
+    if not hasattr(current_app, 'configured'):
+        abort(Response('Docker not configured. Need to call /config API first.'))
+    
+    if request.json and 'classes' in request.json:
+        classes = request.json['classes']
+        if 'inputs' in request.json:
+            inputs = request.json['inputs']
+        elif 'csv_path' in request.json:
+            client_hdfs = InsecureClient('http://nn2:50070')
+            client_hdfs.download(request.json['csv_path'], "./tweets.csv", overwrite=True)
+            inputs = list(pd.read_csv('tweets.csv', names=['tweets']).tweets)
+    else:
+        abort(Response('Json not understandable, make sure you have "inputs" and "classes" key.'))
+
+    
+    # load model and preprocess/postprocess in memory
+    if (not current_app.inMemory):
+        current_app.preprocess = cloudpickle.load(open("./preprocess.pickle", "rb"))
+        current_app.finalModel = cloudpickle.load(open("./model.pickle", "rb"))
+        current_app.inMemory = True
+    logger.info('Load model: OK!!')
+    
+    # Forward pass for predict
+    preproc_inputs = current_app.preprocess(inputs)
+    logger.info('pre-processed inputs: ', preproc_inputs)
+    outputs = current_app.finalModel(preproc_inputs)
+    logger.info('outputs: ', outputs)
+    
+    # output predicted class
+    predictions = np.argmax(outputs.detach().numpy(), axis=1)
+    predictions = [classes[pred] for pred in predictions]
+    
+    return jsonify({'predictions': predictions}), 201
+
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', port=2333)
