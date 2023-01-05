9b9371885cf87a8533c35ef3890df99f1d413dd9
leshanshui-saagie <65134274+leshanshui-saagie@users.noreply.github.com>
2023-01-05 17:43:14 +0100

    Update pred.py

--- pred.py
+++ pred.py
@@ -1,89 +1,78 @@
 import os
 import pandas as pd
 import numpy as np  
+import torch
+from transformers import AdamW, AutoModelForSequenceClassification, AutoTokenizer
+
 from flask import Flask, jsonify, request
 from flask import current_app, abort, Response
-import cloudpickle
-import warnings
-import logging
+
+import warnings, logging
 import subprocess
-import transformers
-from hdfs import InsecureClient
-import importlib
 import requests
 
 
+#%%
+### Logger
 logger = logging.getLogger(__name__)
 handler = logging.StreamHandler()
 handler.setLevel(logging.ERROR)
 
+### Init App
 application = Flask(__name__)
 application.logger.addHandler(handler)
-# logging.basicConfig(level=logging.INFO, format='%(name)s - %(levelname)s - %(message)s')
-# hdfs_uri = 'http://nn1:50070'
 
-@application.route('/config', methods=['POST'])
-def config():
-    # Read params from Json
-    if not request.json or not 'preprocess' in request.json or not 'requirements' in request.json or not 'model' in request.json:
-        abort(400)
-    preprocess_file = request.json['preprocess']
-    requirements_file = request.json['requirements']
-    model_file = request.json['model']
-    hdfs_uri = request.json['hdfs_uri']
-    logger.info('Read json configurations: OK!!')
-    
-    # Download files from HDFS
-    client_hdfs = InsecureClient(hdfs_uri)
-    client_hdfs.download(requirements_file, "./requirements.txt", overwrite=True)
-    client_hdfs.download(model_file, "./model.pickle", overwrite=True)
-    client_hdfs.download(preprocess_file, "./preprocess.pickle", overwrite=True)
-    logger.info('Download pickles: OK!!')
-    
-    # Install library dependencies
-    subprocess.call("pip install -r ./requirements.txt", shell=True)
-    current_app.hdfs_uri = hdfs_uri
-    current_app.inMemory = False
-    current_app.configured = True
-    current_app.client_hdfs = client_hdfs
-    return jsonify({'status': "Docker configured"}), 201
+### Params
+classes = {0:"Negative", 1:"Positive"}
+device = "cpu"
+model_name = 'prajjwal1/bert-tiny'
+model_pt_path = 'model/tinybert_imdb_230105.pth'
+
+### Loading Model and Tokenizer
+model = AutoModelForSequenceClassification.from_pretrained(model_name).to(device)
+state_dict = torch.load(model_pt_path, map_location=device)
+model.load_state_dict(state_dict)
+model.to(device)
+model.eval()
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 
 @application.route('/predict', methods=['POST'])
 def predict():
     # Read inputs/classes or Return reason of abort
-    if not hasattr(current_app, 'configured'):
-        abort(Response('Docker not configured. Need to call /config API first.'))
-    
-    if request.json and 'classes' in request.json:
-        classes = request.json['classes']
-        if 'inputs' in request.json:
-            inputs = request.json['inputs']
-        elif 'csv_path' in request.json:
-            current_app.client_hdfs.download(request.json['csv_path'], "./tweets.csv", overwrite=True)
-            inputs = list(pd.read_csv('tweets.csv', names=['tweets'], skiprows=1, encoding='utf-8', engine='python', error_bad_lines=False).tweets)
+    if 'inputs' in request.json:
+        texts = request.json['inputs']
     else:
-        abort(Response('Json not understandable, make sure that you have "inputs" and "classes" key.'))
+        abort(Response('Json not understandable, make sure that you have "inputs" key.'))
+        
+    ### Preprocessing
+    # Tokenization (code was designed for batch inference, but in real case we have only 1 text in texts list)
+    inputs_mask = [] 
+    texts_ids = []
+    for text in texts: 
+        text_tokenized = tokenizer.tokenize(text.replace('[^\w\s]', ''))
+        text_ids = tokenizer.convert_tokens_to_ids([tokenizer.special_tokens_map["cls_token"]]+text_tokenized+[tokenizer.special_tokens_map["sep_token"]])
+        texts_ids.append(text_ids)
+        inputs_mask.append([1] * len(text_ids))
+    # Batch-Padding (code was designed for batch inference, but in real case we have only 1 text in texts list)
+    lengths = [len(p) for p in texts_ids]
+    max_length = max(lengths)
+    padded_text_ids = np.ones((len(texts_ids), max_length))*tokenizer.convert_tokens_to_ids(tokenizer.special_tokens_map["pad_token"])
+    padded_inputs_mask = np.zeros((len(texts_ids), max_length))
+    for i, l in enumerate(lengths):
+        padded_text_ids[i, 0:l] = texts_ids[i][0:l]
+        padded_inputs_mask[i, 0:l] = inputs_mask[i][0:l]
+    # Transpose to 3 tensors to adapt the input of inference()
+    padded_tensors = torch.LongTensor(np.array([padded_text_ids, padded_inputs_mask]))
     
-    # load model and preprocess/postprocess in memory
-    if (not current_app.inMemory):
-        current_app.preprocess = cloudpickle.load(open("./preprocess.pickle", "rb"))
-        current_app.finalModel = cloudpickle.load(open("./model.pickle", "rb"))
-        current_app.inMemory = True
-    logger.info('Load model: OK!!')
-    
-    # Forward pass for predict
-    preproc_inputs = current_app.preprocess(inputs)
-    logger.info('pre-processed inputs: ', preproc_inputs)
-    outputs = current_app.finalModel(preproc_inputs)
-    logger.info('outputs: ', outputs)
-    
-    # output predicted class
-    predictions = np.argmax(outputs.detach().numpy(), axis=1)
+    ### Prediction
+    text_ids, inputs_mask = padded_tensors
+    outputs = model(text_ids.to(device), inputs_mask.to(device))
+    predictions = np.argmax(outputs.logits.detach().cpu().numpy(), axis=-1)
     predictions = [classes[pred] for pred in predictions]
-    
     return jsonify({'predictions': predictions}), 201
 
 
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', port=2333)
+    application.run(host='0.0.0.0', port=8080)
+    
